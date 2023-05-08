5050b412b880ced92e88e47f668324df73b855e3
Daniel Freitas <bikemessengerateu@gmail.com>
2023-05-08 16:42:59 -0300

    Adicionei o arquivo python com o inicio do projeto
    primeiro passo desse desafio é criar exemplos ficticios de alunos e as respectivas notas, fiz uma lista

---
+++ buscarh.py
@@ -0,0 +1,8 @@
+# Define a lista de candidatos com seus resultados no processo seletivo
+candidatos = [
+    {'nome': 'João', 'resultado': 'e6_t8_p7_s9'},
+    {'nome': 'Maria', 'resultado': 'e8_t7_p9_s8'},
+    {'nome': 'José', 'resultado': 'e4_t6_p8_s7'},
+    {'nome': 'Ana', 'resultado': 'e7_t9_p6_s9'},
+    {'nome': 'Carlos', 'resultado': 'e6_t8_p8_s7'}
+]
