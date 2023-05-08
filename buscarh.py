51ddf5cb7712db0622ade6fa27f124e7701586bb
Daniel Freitas <bikemessengerateu@gmail.com>
2023-05-08 16:59:34 -0300

    Update buscarh.py
    A função itera sobre a lista de candidatos e extrai as notas de cada etapa de cada candidato. Em seguida, ela verifica se as notas de cada etapa são maiores ou iguais às notas de corte fornecidas pelo usuário.
    
    Caso todas as notas sejam maiores ou iguais às notas de corte, o nome do candidato é adicionado a uma lista de candidatos selecionados.
    
    Por fim, a função retorna a lista de candidatos selecionados.
    
    A implementação conta com comentários claros que explicam o que cada parte do código faz, o que torna a leitura e o entendimento mais fáceis para outras pessoas que venham a utilizar ou contribuir com o código.

--- buscarh.py
+++ buscarh.py
@@ -7,5 +7,20 @@ candidatos = [
     {'nome': 'Carlos', 'resultado': 'e6_t8_p8_s7'}
 ]
 def buscar_candidatos(candidatos, nota_e, nota_t, nota_p, nota_s):
-# cria uma lista vazia para armazenar os candidatos selecionados
+    # cria uma lista vazia para armazenar os candidatos selecionados
     candidatos_selecionados = []
+    # itera sobre cada candidato na lista de candidatos fornecida
+    for candidato in candidatos:
+        # obtém o resultado do candidato atual na forma de uma string
+        resultado = candidato['resultado']
+        # extrai as notas de cada etapa do resultado do candidato atual e as converte em inteiros
+        nota_e_candidato = int(resultado[1])
+        nota_t_candidato = int(resultado[4])
+        nota_p_candidato = int(resultado[7])
+        nota_s_candidato = int(resultado[10])
+        # verifica se as notas de cada etapa do candidato atual são maiores ou iguais às notas de corte fornecidas
+        if nota_e_candidato >= nota_e and nota_t_candidato >= nota_t and nota_p_candidato >= nota_p and nota_s_candidato >= nota_s:
+            # adiciona o nome do candidato atual à lista de candidatos selecionados
+            candidatos_selecionados.append(candidato['nome'])
+    # retorna a lista de candidatos selecionados
+    return candidatos_selecionados
